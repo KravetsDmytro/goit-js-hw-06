7a34240f086846abc2b7e4ce83f0bb64f6297297
Kravets_Dmytro <ditur@ukr.net>
2023-03-06 21:28:58 +0200

    завдання 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,15 @@
-const itenS =document.querySelector(ul#categories)
-console.log(itenS)
+// рахує і виведе в консоль кількість категорій
+//  в ul#categories, тобто елементів li.item.
+
+const categoriesItems = document.querySelectorAll('.item');
+console.log(`Number of categories:  ${categoriesItems.length}`);
+
+
+categoriesItems.forEach((element) => {
+    const titleEl = element.querySelector('h2').textContent;
+    const liElement = element.querySelectorAll('li').length;
+    console.log(`Category: ${titleEl}`) ;
+    console.log(`Elements: ${liElement}`);
+})
+
+
