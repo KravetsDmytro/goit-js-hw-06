f875a0d4fb7fbc68522ed3f70618bed7d3e4c3cf
Kravets_Dmytro <ditur@ukr.net>
2023-03-19 17:30:19 +0200

    task 10

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,39 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const refs ={ 
+//   body: document.querySelector('body'),
+//   span: document.querySelector('.color'),
+//   button :  document.querySelector('.change-color')};
+
+//   console.log(refs)
+  
+// refs.button.addEventListener('click', () => {
+//   const randomColor = getRandomHexColor();
+//   refs.body.style.backgroundColor = randomColor;
+//   refs.span.textContent = randomColor;
+// })
+
+const button = document.querySelector(".change-color");
+
+const textEl = document.querySelector(".color");
+
+const bodyEl = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+button.addEventListener("click", onButtonChange);
+
+function onButtonChange(event) {
+  const ColorEl = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = ColorEl;
+  textEl.textContent = ColorEl;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,44 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// створити функцію яка створює бокс 30на 30 і збільшує на 10 . 
+
+
+function createBoxes(amount) {
+  let parameterOfCreatedElement = 70;
+  const arrOfCreatedElements = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const createdElement = document.createElement("div");
+    createdElement.style.width = parameterOfCreatedElement + "px";
+    createdElement.style.height = parameterOfCreatedElement + "px";
+    parameterOfCreatedElement += 10;
+    // зміна колору за функц.
+    createdElement.style.backgroundColor = getRandomHexColor();
+    arrOfCreatedElements.push(createdElement);
+    console.log (createdElement)
+  }
+console.log (arrOfCreatedElements)
+  return arrOfCreatedElements;
+};
+
+// пошук  та присвоєння змінних 
+const inputNumber = document.querySelector("[type = 'number']");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+// відловлення подій  при кліці
+createBtn.addEventListener("click", onCreate);
+destroyBtn.addEventListener("click", onDestroy);
+
+
+function onCreate(event) {
+  const elements = createBoxes(inputNumber.value);
+  console.log(elements)
+  boxes.append(...elements);
+}
+
+function onDestroy(event) {
+  boxes.innerHTML = "";
+}
